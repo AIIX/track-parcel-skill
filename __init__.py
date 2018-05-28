6e5f7ab6416cf25784bf10421ff781c8b0fadbd4
Aditya Mehra <Aix.m@outlook.com>
2018-05-29 00:41:46 +0930

    Fix Finding Slug

--- __init__.py
+++ __init__.py
@@ -9,7 +9,9 @@ from os.path import dirname, abspath
 from adapt.intent import IntentBuilder
 from mycroft.util.log import getLogger
 from mycroft.skills.core import MycroftSkill, intent_handler
+from mycroft.messagebus.message import Message
 from mycroft.skills.context import *
+from word2number import w2n
 
 __author__ = 'aix'
 
@@ -36,39 +38,73 @@ class TrackParcelSkill(MycroftSkill):
         self.speak('What Courier Service Would You Like To Track ?', expect_response=True)
     
     @intent_handler(IntentBuilder("TrackParcelCourierInfoIntent").require("TrackParcelCourierKeyword").require("TrackContext").build())
-    @adds_context('CourierContext')
     def handle_tracking_courierinfo_intent(self, message):
         """
         Get Courier Info
         """
+        global courierService
+        global courierLists
+        global listofcouriers
         utterance = message.data.get('utterance').lower()
+        with open(self.couriers_index) as json_data:
+                courierLists = json.load(json_data)
+        
         
         def findSlug(keywords):
             keyword = keywords.lower()
-            with open(self.couriers_index) as json_data:
-                d = json.load(json_data)
-                for keys, value in d:
-                    self.speak("a");
-                    if keyword in value:
-                        self.speak(key)
-                        return key
-                    else:
-                        return keyword
-                    
-        serviceSlug = findSlug(utterance)
-        global courierService
-        courierService = serviceSlug
-        self.speak('Please Speak Your Tracking Number After This Message', expect_response=True)
-    
+            listofcouriers = []
+            for key, value in courierLists.items():
+                if value.find(keyword) != -1:
+                    listofcouriers.append(value)
+            return listofcouriers
+        
+        listofcouriers = findSlug(utterance)
+        if len(listofcouriers) > 1:
+            for idx, courier in enumerate(listofcouriers):
+                self.speak(str(idx + 1) + ' ' + courier)
+            self.speak('Please choose a number', expect_response=True)
+            self.set_context('SelectContext')
+            
+        elif len(listofcouriers) == 1:
+            for key,value in courierLists.items():
+                if(listofcouriers[0] == value):
+                    LOGGER.info(listofcouriers[0])
+                    courierService = listofcouriers[0]
+            self.speak('Please Speak Your Tracking Number After This Message', expect_response=True)
+            self.set_context('CourierContext')
+        else:
+            self.speak('Sorry no courier found')
+
+    @intent_handler(IntentBuilder("TrackParcelMultipleSelectionIntent").require("TrackParcelSelectionNumber").require("SelectContext").build())
+    @adds_context("CourierContext")
+    def handle_tracking_multiple_selection(self, message):
+        utterance = message.data.get('TrackParcelSelectionNumber').lower()
+        selectNumber = w2n.word_to_num(utterance)
+        if(len(listofcouriers[selectNumber - 1]) > 1):
+            for key,value in courierLists.items():
+                if listofcouriers[selectNumber - 1] == value :
+                    courserService = key
+            self.speak('Please Speak Your Tracking Number After This Message', expect_response=True)
+            self.set_context("CourierContext")
+        else: 
+            self.speak('Sorry your selected number was not found')
+        
+        
     @intent_handler(IntentBuilder("TrackParcelResultIntent").require("TrackParcelNumberKeyword").require("CourierContext").build())
     def handle_tracking_result_intent(self, message):
         """
         Find Package Details
-        """
-        utterance = message.data.get('utterance').lower()
-        trackingNumber = utterance
-        api = aftership.APIv4('eae7efb7-6ac4-4b89-aeec-e620294cad33')
-        
+        """                
+        def extractNum(splitUtterance):
+            for word in splitUtterance:
+                try:
+                    extractedNumber = w2n.word_to_num(word)
+                    return extractedNumber
+                
+                except:
+                    isWord = word
+                    self.speak("error here")
+                    
         def addTrackingDetails(courierService, trackingNumber):
             self.speak("Searching for your package, Please wait")
             resultdetail = api.trackings.post(tracking=dict(slug=courierService, tracking_number=trackingNumber, title="Title"))
@@ -90,6 +126,20 @@ class TrackParcelSkill(MycroftSkill):
 
         def removeTrackingDetails(courierService, trackingNumber):
             r = api.trackings.delete(courierService, trackingNumber)
+
+        utterance = message.data.get('utterance').lower()
+        splitUtterance = utterance.split()
+        convertedList = []
+        for idx, word in enumerate(splitUtterance):
+            try:
+                LOGGER.info('Split' + word)
+                convertedList.append(w2n.word_to_num(word))
+            except:
+                convertedList.append(word)
+
+        LOGGER.info(convertedList)
+        trackingNumber = ''.join(convertedList)
+        api = aftership.APIv4('eae7efb7-6ac4-4b89-aeec-e620294cad33')
 
         addTrackingDetails(courierService, trackingNumber)
 
